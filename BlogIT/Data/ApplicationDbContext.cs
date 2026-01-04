7fa01e00f7f9f3e735af52040638eb1840ae8cb0
Alex Kane <alexfitzkane@gmail.com>
2026-01-04 13:52:50 -0500

    added nuget packages to support azure vault connection. added dbcontext and linked it with azure vault. added Category model and tested migration with success.

---
+++ BlogIT/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogIT.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogIT.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        DbSet<Category> Categories { get; set; }
+    }
+}
--- BlogIT/Program.cs
+++ BlogIT/Program.cs
@@ -1,3 +1,6 @@
+using Azure;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
@@ -5,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.SwaggerUI;
+using BlogIT.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +18,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+var keyVaultName = builder.Configuration["KeyVault:KeyVaultName"];
+var keyVaultUrl = new Uri($"https://{keyVaultName}.vault.azure.net/");
+
+builder.Configuration.AddAzureKeyVault(
+    keyVaultUrl,
+    new DefaultAzureCredential()
+);
+
+var connectionSring = builder.Configuration.GetConnectionString("BlogITDevConnection")!;
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionSring);
+});
 
 var app = builder.Build();
 
@@ -32,6 +54,16 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
+
+//app.MapGet("secrets", async (IConfiguration configuration) =>
+//{
+//    var secretsClient = new SecretClient(
+//        new Uri(configuration["KeyVault:KeyVaultUri"]!),
+//        new DefaultAzureCredential());
+
+//    Response<KeyVaultSecret> response = await secretsClient.GetSecretAsync("BlogITDevConnection");
+//    return Results.Ok(response);
+//});
 
 app.UseAuthorization();
 
